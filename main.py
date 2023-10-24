32820ff8776347fb9372ad1c02577d3796a5bf30
Mrjpz <38113524+Mrjpz@users.noreply.github.com>
2023-10-24 17:40:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-import visualizer
+import graph_function
 from randomizer import numbers
 from typing import Optional
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLineEdit
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     
     application = QApplication(sys.argv)
     
-    window = visualizer.Visualizer()
+    window = graph_function.Visualizer() #from visualizer import Visualizer()
     window.show()
     window.resize(1920, 1080)
     
