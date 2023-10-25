e60392d9f3ef1d94bf7e6408392a8a2103377230
Mrjpz <38113524+Mrjpz@users.noreply.github.com>
2023-10-24 22:05:31 -0400

    Added some line graphs

--- graph_function.py
+++ graph_function.py
@@ -1,5 +1,8 @@
-
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLineEdit
+import sys, main
+from PySide6.QtCore import Qt, QRectF, Slot
+from PySide6.QtGui import QBrush, QColor, QPainter, QPen
+from PySide6.QtWidgets import QApplication, QDoubleSpinBox, QFormLayout, QGridLayout, QGroupBox, QPushButton, QWidget, QMainWindow, QVBoxLayout, QLineEdit
+from PySide6.QtCharts import QBarSeries, QBarSet, QChart, QChartView
 
 
 
@@ -9,12 +12,37 @@ class Visualizer(QMainWindow):
         self.UI()
 
     def UI(self):
-        layout = QVBoxLayout()
+        w = Visualizer.screen()
+        print(w)
         
-        self.textbox = QLineEdit(self)
-        layout.addWidget(self.textbox)
-
-        self.setLayout(layout)
+        
+        
+        #main layout
+        self.main_layout = QGridLayout()
+        self.settings_layout = QGridLayout()
+        #-----
+        
+        self.chart = QChart()
+        self.barseries = QBarSeries()
+        self.barset = QBarSet(None)
+        
+        self.barset.append([8,8,12,3,6,4,2,3,4,1,2])
+        self.barseries.append(self.barset)
+        self.chart.addSeries(self.barseries)
+        
+        self.chartview = QChartView(self.chart, self)
+        
+        
+        self.chartview.setMinimumSize(720,440)
+        
+        
+        #-----
+        
+        self.main_layout.addWidget(self.chartview, 0, 1, 3, 1)
+        
+        self.setLayout(self.main_layout)
         
         self.setWindowTitle("Sorting Visualizer")
         self.show()
+
+    
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-import graph_function
+import sys, graph_function
 from randomizer import numbers
 from typing import Optional
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLineEdit
@@ -9,11 +8,10 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, Q
 
 if __name__ == '__main__':
     
-    application = QApplication(sys.argv)
-    
-    window = graph_function.Visualizer() #from visualizer import Visualizer()
-    window.show()
-    window.resize(1920, 1080)
-    
-    
-    sys.exit(application.exec())
+    app = QApplication(sys.argv)
+    w = graph_function.Visualizer()
+    available_geometry = w.screen().availableGeometry()
+    size = available_geometry.height() * .75
+    w.setFixedSize(size, size)
+    w.show()
+    sys.exit(app.exec())
